c365ae519ed5f05350c40ac9dede1daab77ab47c
pdelbert <pmarcadel@gmail.com>
2024-08-21 10:49:16 -0300

    Adding - Constants File

---
+++ constants/request.ts
@@ -0,0 +1,8 @@
+export const ID_TEMPLATE = '{0}';
+
+export const PRODUCTS_ENDPOINT = {
+    BASE: 'products',
+    ATTIBUTES: 'products/attributes',
+    CATEGORIES: 'products/categories',
+    VARIATION: 'products/{0}/variations'
+}
--- model/productModel.ts
+++ model/productModel.ts
@@ -1,69 +1,73 @@
-import { wooCommerceAdd, wooCommerceDeleteById, wooCommerceFindAll, wooCommerceFindById, wooCommerceUpdateById } from "../services/WooCommerce.service";
+import { ID_TEMPLATE, PRODUCTS_ENDPOINT } from "../constants/request";
 
-const PATH_PRODUCTS = 'products';
-const PATH_PRODUCTS_ATTIBUTES = 'products/attributes'
-const PATH_PRODUCTS_CATEGORIES = 'products/categories'
-const PATH_PRODUCTS_VARIATION = 'products/{0}/variations'
+import { 
+    wooCommerceAdd, 
+    wooCommerceFindAll, 
+    wooCommerceFindById, 
+    wooCommerceUpdateById 
+} from "../services/WooCommerce.service";
+
+const { BASE, VARIATION, ATTIBUTES, CATEGORIES } = PRODUCTS_ENDPOINT;
 
 
 // PRODUCTS
 export const getAllProducts = async() => {
-    return await wooCommerceFindAll(PATH_PRODUCTS);
+    return await wooCommerceFindAll(BASE);
 }
 
 export const getProduct = async(id:string) => {
-    return await wooCommerceFindById(PATH_PRODUCTS, id);
+    return await wooCommerceFindById(BASE, id);
 }
 
 export const createProduct = async(data:string) => {
-    return await wooCommerceAdd(PATH_PRODUCTS, data);
+    return await wooCommerceAdd(BASE, data);
 }
 
 export const updateProduct = async(id:string, data:string) => {
-    return await wooCommerceUpdateById(PATH_PRODUCTS, id, data);
+    return await wooCommerceUpdateById(BASE, id, data);
 }
 
 // PRODUCTS VARIATION
 export const productVariation = async(id:string, data:string) => {
-    const PATH = PATH_PRODUCTS_VARIATION.replace('{0}',id);
+    const PATH = VARIATION.replace(ID_TEMPLATE, id);
     return await wooCommerceAdd(PATH, data);
 }
 
 export const allProductsVariation = async(id:string) => {
-    const PATH = PATH_PRODUCTS_VARIATION.replace('{0}',id);
+    const PATH = VARIATION.replace(ID_TEMPLATE, id);
     return await wooCommerceFindAll(PATH);
 }
 
 // PRODUCTS ATTRIBUTES
 export const productAttributes = async(data:string) => {
-    return await wooCommerceAdd(PATH_PRODUCTS_ATTIBUTES, data);
+    return await wooCommerceAdd(ATTIBUTES, data);
 }
 
 export const allProductsAttributes = async() => {
-    return await wooCommerceFindAll(PATH_PRODUCTS_ATTIBUTES);
+    return await wooCommerceFindAll(ATTIBUTES);
 }
 
 export const findProductAttributeById = async(id:string) => {
-    return await wooCommerceFindById(PATH_PRODUCTS_ATTIBUTES, id);
+    return await wooCommerceFindById(ATTIBUTES, id);
 }
 
 export const changeProductAttributeById = async(id:string, data:string) => {
-    return await wooCommerceUpdateById(PATH_PRODUCTS_ATTIBUTES, id, data);
+    return await wooCommerceUpdateById(ATTIBUTES, id, data);
 }
 
 // PRODUCTS CATEGORIES
 export const allProductCategories = async() => {
-    return await wooCommerceFindAll(PATH_PRODUCTS_CATEGORIES);
+    return await wooCommerceFindAll(CATEGORIES);
 }
 
 export const findProductCategoriesById = async(id:string) => {
-    return await wooCommerceFindById(PATH_PRODUCTS_CATEGORIES, id);
+    return await wooCommerceFindById(CATEGORIES, id);
 }
 
 export const productCategories = async(data:string) => {
-    return await wooCommerceAdd(PATH_PRODUCTS_CATEGORIES, data);
+    return await wooCommerceAdd(CATEGORIES, data);
 }
 
 export const changeProductCategories = async(id:string, data:string) => {
-    return await wooCommerceUpdateById(PATH_PRODUCTS_CATEGORIES, id, data);
+    return await wooCommerceUpdateById(CATEGORIES, id, data);
 }
